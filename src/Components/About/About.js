31ed6b0ccf7c7d3e408790dba4faa03d8949b6d7
Nazirul-cloud <shanto2193@gmail.com>
2021-10-05 16:19:25 +0600

    final edit

--- src/Components/About/About.js
+++ src/Components/About/About.js
@@ -2,12 +2,12 @@ import React from 'react';
 import about from '../../images/about.jpg';
 const About = () => {
     return (
-        <div className="d-flex">
-            <div className="text-center">
+        <div className="d-flex mt-5">
+            <div className="text-center mt-5 mb-5">
                 <h2>About The Instructor (James)</h2>
                 <p>I first picked up a guitar at age 17. I was at a friend’s house. He showed me how to play the riff from “Smells Like Teen Spirit,” and I was immediately hooked. Several thousand hours of practice, performance and one music degree later, I found myself wondering “what next?”</p>
             </div>
-            <div>
+            <div className="mt-4 p-3">
                 <img src={about} alt="" width="150"
                 height="150" />
             </div>
--- src/Components/Contact/Contact.js
+++ src/Components/Contact/Contact.js
@@ -2,12 +2,12 @@ import React from 'react';
 
 const Contact = () => {
     return (
-        <div>
-            <p className="text-center p-4">If you would like to contact us via email then please fill in the form below. We'll get back to you either by phone, text or email, so let us know your preference. Speak to you soon!</p>
+        <div className="mt-5">
+            <p className="text-center p-4">If you would like to contact us via email then please fill in the form below. <br /> We'll get back to you either by phone, text or email, so let us know your preference. Speak to you soon!</p>
             <div className="text-center">
-                <input className="w-50 b" type="text" />
+                <input className="w-50 rounded-pill" type="text" />
                 <br/>
-                <button className="btn btn-info mt-2">Send</button>
+                <button className="btn btn-info mt-2 rounded-pill ">Send</button>
             </div>
         </div>
     );
